dbe2d49717085e10c284a082d1fe4e28dadf73c2
Ruben <rwoldhui@avans.nl>
2019-12-30 19:47:51 +0100

    [Fixed] closing on null

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -174,7 +174,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        sessionManager.close();
+        if (sessionManager != null)
+            sessionManager.close();
         sessionManager = null;
     }
 
